a4fa7095fac4cba6973d0e77d8f47b96c772b17b
Ctbenoit60 <ctbenoit60@gmail.com>
2024-01-13 15:43:18 -0600

    Added Calendar, homepage, and about page

---
+++ PersonalizeCal/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function Homepage() { 
+    return (
+    <div className="Homepage">
+    <h1>Personalize</h1>
+    <p>Personalize your digital calendar.</p>
+    <button>Login</button>
+    </div>
+    )
+    }
