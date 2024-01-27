3accfd0a7753c86583c324ec4b9ebb96a3c8ed93
Ctbenoit60 <ctbenoit60@gmail.com>
2024-01-27 14:56:26 -0600

    Created a SignIN component to run through the Login Page

--- PersonalizeCal/src/pages/LoginPage.jsx
+++ PersonalizeCal/src/pages/LoginPage.jsx
@@ -1,28 +1,9 @@
-// import { Button } from '@mui/material';
-// import { useNavigate } from "react-router-dom";
-
-// export default function LoginPage() {
-//     let navigate = useNavigate();
-//   const routeChange = () =>{
-//     let path = `/Calendar`;
-//     navigate(path);
-
-// }
-//   return (
-//   <div className="Homepage">
-//   <h1>Personalize</h1>
-//   <img src="/src/assets/logo.png" alt="Personalize Logo"
-//   width={"300px"} height={"auto"} />
-//   <br />
-//   <p>Personalize your digital calendar.</p>
-//   <Button size="small" color="primary" onClick={routeChange}> Login </Button>
-//   </div>
-//   )
-//   }
 
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import SignIn from "../components/LoginSignUp";
+
 
 function LoginPage() {
   const [username, setUsername] = useState("");
@@ -53,34 +34,7 @@ function LoginPage() {
         />
         <br />
         <p>Personalize your digital calendar.</p>
-        <TextField
-          id="username"
-          label="Username"
-          variant="outlined"
-          value={username}
-          onChange={handleUsernameChange}
-          fullWidth
-          margin="normal"
-          required
-        />
-      </div>
-      <div>
-        <TextField
-          id="password"
-          label="Password"
-          variant="outlined"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          fullWidth
-          margin="normal"
-          required
-        />
-      </div>
-      <div>
-        <Button type="submit" variant="contained" color="primary">
-          Sign In
-        </Button>
+        <SignIn></SignIn>
       </div>
     </form>
   );
